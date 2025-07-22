2b051485ea77b68f64a11a206277b0aa4335a07e
KimLopesBraatz <wanzeller.scabbia@gmail.com>
2025-07-22 12:18:28 -0300

    fix/producer-max-win Fix query for max consecutive wins

--- src/main/java/br/com/outsera/gra_service/adapters/outbound/repository/producer/ProducerJpaRepository.java
+++ src/main/java/br/com/outsera/gra_service/adapters/outbound/repository/producer/ProducerJpaRepository.java
@@ -23,26 +23,50 @@ public interface ProducerJpaRepository extends JpaRepository<ProducerJpaEntity, 
             nativeQuery = true)
     List<ProducerConsecutiveWinProjection> findProducerConsecutiveWinsWithIntervalMin();
 
-    @Query(value = "SELECT p.NAM_NAME as producer, m1.DAT_YEAR as previousWin, m2.DAT_YEAR as followingWin, (m2.DAT_YEAR - m1.DAT_YEAR) as `interval` " +
-            "FROM GRA_PRODUCER p " +
-            "JOIN GRA_MOVIE_PRODUCER mp1 ON p.IDT_PRODUCER = mp1.IDT_PRODUCER " +
-            "JOIN GRA_MOVIE m1 ON mp1.IDT_MOVIE = m1.IDT_MOVIE " +
-            "JOIN GRA_MOVIE_PRODUCER mp2 ON p.IDT_PRODUCER = mp2.IDT_PRODUCER " +
-            "JOIN GRA_MOVIE m2 ON mp2.IDT_MOVIE = m2.IDT_MOVIE " +
-            "WHERE m1.FLG_WINNER = true AND m2.FLG_WINNER = true " +
-            "AND m1.IDT_MOVIE <> m2.IDT_MOVIE " +
-            "AND m2.DAT_YEAR > m1.DAT_YEAR " +
-            "AND (m2.DAT_YEAR - m1.DAT_YEAR) = (" +
-            "   SELECT MAX(m2b.DAT_YEAR - m1b.DAT_YEAR) " +
-            "   FROM GRA_PRODUCER pb " +
-            "   JOIN GRA_MOVIE_PRODUCER mp1b ON pb.IDT_PRODUCER = mp1b.IDT_PRODUCER " +
-            "   JOIN GRA_MOVIE m1b ON mp1b.IDT_MOVIE = m1b.IDT_MOVIE " +
-            "   JOIN GRA_MOVIE_PRODUCER mp2b ON pb.IDT_PRODUCER = mp2b.IDT_PRODUCER " +
-            "   JOIN GRA_MOVIE m2b ON mp2b.IDT_MOVIE = m2b.IDT_MOVIE " +
-            "   WHERE m1b.FLG_WINNER = true AND m2b.FLG_WINNER = true " +
-            "   AND m1b.IDT_MOVIE <> m2b.IDT_MOVIE " +
-            "   AND m2b.DAT_YEAR > m1b.DAT_YEAR" +
-            ") ",
+    @Query(value = "SELECT * " +
+            "FROM (" +
+            "    SELECT p.NAM_NAME AS producer, m1.DAT_YEAR AS previousWin, m2.DAT_YEAR AS followingWin, (m2.DAT_YEAR - m1.DAT_YEAR) AS `interval` " +
+            "    FROM GRA_PRODUCER p " +
+            "    JOIN GRA_MOVIE_PRODUCER mp1 ON p.IDT_PRODUCER = mp1.IDT_PRODUCER " +
+            "    JOIN GRA_MOVIE m1 ON mp1.IDT_MOVIE = m1.IDT_MOVIE " +
+            "    JOIN GRA_MOVIE_PRODUCER mp2 ON p.IDT_PRODUCER = mp2.IDT_PRODUCER " +
+            "    JOIN GRA_MOVIE m2 ON mp2.IDT_MOVIE = m2.IDT_MOVIE " +
+            "    WHERE " +
+            "        m1.FLG_WINNER = true AND m2.FLG_WINNER = true " +
+            "        AND m2.DAT_YEAR > m1.DAT_YEAR " +
+            "        AND NOT EXISTS ( " +
+            "            SELECT 1 " +
+            "            FROM GRA_MOVIE_PRODUCER mp3 " +
+            "            JOIN GRA_MOVIE m3 ON mp3.IDT_MOVIE = m3.IDT_MOVIE " +
+            "            WHERE " +
+            "                mp3.IDT_PRODUCER = p.IDT_PRODUCER " +
+            "                AND m3.FLG_WINNER = true " +
+            "                AND m3.DAT_YEAR > m1.DAT_YEAR " +
+            "                AND m3.DAT_YEAR < m2.DAT_YEAR " +
+            "        ) " +
+            ") intervals " +
+            "WHERE intervals.`interval` = ( " +
+            "    SELECT MAX(m2.DAT_YEAR - m1.DAT_YEAR) " +
+            "    FROM GRA_PRODUCER p " +
+            "    JOIN GRA_MOVIE_PRODUCER mp1 ON p.IDT_PRODUCER = mp1.IDT_PRODUCER " +
+            "    JOIN GRA_MOVIE m1 ON mp1.IDT_MOVIE = m1.IDT_MOVIE " +
+            "    JOIN GRA_MOVIE_PRODUCER mp2 ON p.IDT_PRODUCER = mp2.IDT_PRODUCER " +
+            "    JOIN GRA_MOVIE m2 ON mp2.IDT_MOVIE = m2.IDT_MOVIE " +
+            "    WHERE " +
+            "        m1.FLG_WINNER = true AND m2.FLG_WINNER = true " +
+            "        AND m2.DAT_YEAR > m1.DAT_YEAR " +
+            "        AND NOT EXISTS ( " +
+            "            SELECT 1 " +
+            "            FROM GRA_MOVIE_PRODUCER mp3 " +
+            "            JOIN GRA_MOVIE m3 ON mp3.IDT_MOVIE = m3.IDT_MOVIE " +
+            "            WHERE " +
+            "                mp3.IDT_PRODUCER = p.IDT_PRODUCER " +
+            "                AND m3.FLG_WINNER = true " +
+            "                AND m3.DAT_YEAR > m1.DAT_YEAR " +
+            "                AND m3.DAT_YEAR < m2.DAT_YEAR " +
+            "        ) " +
+            ") " +
+            "ORDER BY producer, previousWin",
             nativeQuery = true)
     List<ProducerConsecutiveWinProjection> findProducerConsecutiveWinsWithMaxInterval();
 }
